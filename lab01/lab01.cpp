27b5d106db91a0d93555134eb905e1bcb2ec02b1
markmccreary00 <m274230@usna.edu>
2024-01-11 14:37:43 -0500

    msg

---
+++ lab01/lab01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!" << endl;
+   cout << "Hello Solar System!" << endl  ;
+   cout << "Hello Galaxy!" << endl  ;
+   cout << "Hello Universe!" << endl  ;
+   cout << "Hello ... ?" << endl  ;
+   cout << "This is a 1!" << endl  ;
+   cout << "This is a 2!" << endl  ;
+   cout << "This is a 3!" << endl  ;
+   cout << "This is a 4!" << endl  ;
+   cout << "This is a 5!" << endl  ;
+
+   return 0;
+}

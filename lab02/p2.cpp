f98fe1c17e1c2247f48d5665c8eb10d1661849aa
markmccreary00 <m274230@usna.edu>
2024-01-18 15:19:36 -0500

    msg

--- lab02/p2.cpp
+++ lab02/p2.cpp
@@ -17,30 +17,29 @@ int main()
     //Compute value of MSB
     int b4_msb_dec;
     bool b4_msb_bool;
-    b4_msb_dec = (user_input
+    b4_msb_dec = 
     b4_msb_bool = b4_msb_dec;
 
     //Compute value of bit 3
     int b3_dec;
     bool b3_bool;
-    b3_dec = (user_input
+    b3_dec = 
     b3_bool = b3_dec;
 
     //Compute value of bit 2
     int b2_dec;
     bool b2_bool;
-    b2_dec = (user_input
+    b2_dec = 
     b2_bool = b2_dec; 
 
     //Compute value of LSB
     int b1_lsb_dec;
     bool b1_lsb_bool;
-    b1_lsb_dec = (user_input
-    b1_lsb_bool = b2_dec;
+    b1_lsb_dec = 
+    b1_lsb_bool = b1_lsb_dec;
 
-    cout << "First three bits are " << b4_msb_bool << b3_bool << b2_bool << endl;
-
-   
+    //Return binary value to user
+    cout << user_input << " in binary is " << b4_msb_bool << b3_bool << b2_bool << b1_lsb_bool << endl;
 
     return 0;
 }

f614e206753f67609f0380f65aa3d38fba287c12
markmccreary00 <m274230@usna.edu>
2024-02-15 19:32:10 -0500

    msg

--- Practicum/p3.cpp
+++ Practicum/p3.cpp
@@ -14,55 +14,82 @@ int main(){
 
     
     for(int r=0;r<(num/2);r++){
+        
         for(int j=0;j<r;j++)
             cout << "."; 
+
         cout << "@";
+
         for(int j=0;j<(num-(2*r)-2);j++)
             cout << ".";
+
         cout << "@";
+
         for(int j=0;j<r;j++)
             cout << ".";
+
         cout << endl;
+
     }
 
     if(num % 2 != 0){
+
         for(int j=0;j<(num/2);j++)
             cout << ".";
+
         cout << "@";
+
         for(int j=0;j<(num/2);j++)
             cout << ".";
+
         cout << endl;
 
         for(int r=0;r<(num/2);r++){
-        for(int j=0;j<((num/2)-(r+1));j++)
-            cout << "."; 
-        cout << "@";
-        for(int j=0;j<((2*r)+1);j++)
-            cout << ".";
-        cout << "@";
-        for(int j=0;j<((num/2)-(r+1));j++)
-            cout << ".";
-        cout << endl;
+
+            for(int j=0;j<((num/2)-(r+1));j++)
+                cout << "."; 
+
+            cout << "@";
+
+            for(int j=0;j<((2*r)+1);j++)
+                cout << ".";
+
+            cout << "@";
+
+            for(int j=0;j<((num/2)-(r+1));j++)
+                cout << ".";
+
+            cout << endl;
         }
     }
     else{
+
         for(int j=0;j<((num/2)-1);j++)
             cout << ".";
+
         cout << "@@";
+
         for(int j=0;j<((num/2)-1);j++)
             cout << ".";
+
         cout << endl;
 
         for(int r=0;r<((num/2)-1);r++){
-        for(int j=0;j<((num/2)-(r+2));j++)
-            cout << "."; 
-        cout << "@";
-        for(int j=0;j<((2*r)+2);j++)
-            cout << ".";
-        cout << "@";
-        for(int j=0;j<((num/2)-(r+2));j++)
-            cout << ".";
-        cout << endl;
+
+            for(int j=0;j<((num/2)-(r+2));j++)
+                cout << "."; 
+            
+            cout << "@";
+
+            for(int j=0;j<((2*r)+2);j++)
+                cout << ".";
+
+            cout << "@";
+
+            for(int j=0;j<((num/2)-(r+2));j++)
+                cout << ".";
+
+            cout << endl;
     }
     }
 
